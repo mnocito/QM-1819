0bdfc7d871d77ba10c797ceb3bf614e74cce6ed5
Marco Nocito <mnocito@gmail.com>
2018-11-06 23:31:37 -0500

    renamed tensorflow test for clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TensorFlowOpmodeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TensorFlowOpmodeTest.java
@@ -51,8 +51,8 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-public class ConceptTensorFlowObjectDetection extends LinearOpMode {
+@TeleOp(name = "TensorFlow Opmode Test", group = "Concept")
+public class TensorFlowOpmodeTest extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
     private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
     private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
